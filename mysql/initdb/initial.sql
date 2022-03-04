f5de4c9a4ad8c15a46c59ccaf193155e155b796d
Marcus Oliveira <killertiger@gmail.com>
2022-03-04 08:47:47 -0300

    first commit

---
+++ mysql/initdb/initial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE people (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255)
+);
+
+INSERT INTO people(name) values('Marcus');
+INSERT INTO people(name) values('Maria');
+INSERT INTO people(name) values('Jose');
---
+++ node/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const mysql = require('mysql')
+
+
+
+app.get('/', (req, res) => {
+      const connection = mysql.createConnection(config)
+
+      connection.query("SELECT * FROM people", function(err, result, fields) {
+        let html = "";
+
+        for(let i=0; i < result.length; i++)
+        {
+          html += "<p>" + result[i].name + "</p>"
+        }
+
+        res.send('<h1>Full cycle</h1><div>' + html + '</div>')
+    })
+
+    connection.end()
+})
+
+app.listen(port, () => {
+  console.log('Rodando na porta ' + port)  
+})
